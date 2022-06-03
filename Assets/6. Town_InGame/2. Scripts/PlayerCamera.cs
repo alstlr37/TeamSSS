b5ffacdbc40596e0bd89ba61808a0dfa172f9b3d
chansik0504 <chansik0504@gmail.com>
2022-06-03 17:58:40 +0900

    mn m

---
+++ Assets/6. Town_InGame/2. Scripts/PlayerCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public GameObject player;
+
+    private void Update()
+    {
+        this.transform.position = player.transform.position;
+    }
+}
